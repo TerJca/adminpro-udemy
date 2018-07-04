e45231ce8c5297ffac752b3f99e9d3ffcfa51ebf
Jose Azorin <jcazorin@teralco.com>
2018-07-04 10:37:14 +0200

    Input, output y viewchild

---
+++ src/app/components/grafico-dona/grafico-dona.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-grafico-dona',
+  templateUrl: './grafico-dona.component.html'
+})
+
+export class GraficoDonaComponent implements OnInit {
+
+  @Input() data: string[] = [];
+  @Input() labels: number[] = [];
+  @Input() chartType: string = '';
+
+  constructor()
+  {
+    console.log(this.data);
+  }
+
+  ngOnInit()
+  {
+    console.log(this.data);
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,18 +4,31 @@ import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 
+// ng2-charts
+import { ChartsModule } from 'ng2-charts';
+
+// Temporal
+
 // Modulos
 import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+
+// Components
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 // Routes
 import { PAGES_ROUTES } from './pages.routes';
+
 
 @NgModule({
     declarations: [
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
-        PagesComponent
+        PagesComponent,
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
     exports: [
         DashboardComponent,
@@ -25,7 +38,9 @@ import { PAGES_ROUTES } from './pages.routes';
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
     ]
 })
 
